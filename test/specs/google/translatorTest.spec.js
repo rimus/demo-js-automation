8ba3199e4ca4a1113e402c628013d855defe1363
Aliaksandr Yeutushkou <aliaksandr_yeutushkou@epam.com>
2020-07-23 12:55:21 +0300

    Update specs

--- test/specs/google/translatorTest.spec.js
+++ test/specs/google/translatorTest.spec.js
@@ -1,14 +1,18 @@
 const TranslatorPage = require('../../../pages/google/translatePage');
 const {describe, it} = require('mocha');
 const assert = require('assert');
+const allureReporter = require('@wdio/allure-reporter').default;
 
-describe('Google', function () {
+allureReporter.addStory('Google Translator Test Suite');
+describe('Google Translator', function () {
+	allureReporter.addFeature('Translate "geschwindigkeit" to "speed"');
 	it('should translate "geschwindigkeit" to "speed"', async function () {
 		const page = new TranslatorPage();
 		await page.openPage('https://translate.google.com');
 		await page.typeTextToTranslate('geschwindigkeit');
 		const actualTranslation = (await page.getTranslation()).toLowerCase();
 		const expectedTranslation = 'speed';
+		allureReporter.addStep(`Verify that translation is correct`);
 		assert.strictEqual(actualTranslation, expectedTranslation);
 	});
 });
--- test/specs/greenTest.spec.js
+++ test/specs/greenTest.spec.js
@@ -1,13 +1,17 @@
 const Page = require('../../pages/page');
 const {describe, it} = require('mocha');
 const assert = require('assert');
+const allureReporter = require('@wdio/allure-reporter').default;
 
+allureReporter.addStory('Green Test');
 describe('EPAM homepage', function () {
+	allureReporter.addFeature('Page title');
 	it('should have the right title', async function () {
 		const page = new Page();
 		await page.openPage('https://www.epam.com/');
 		const actualTitle = await page.getTitle();
 		const expectedTitle = 'EPAM | Enterprise Software Development, Design & Consulting';
+		allureReporter.addStep(`Verify that page title is correct`);
 		assert.strictEqual(actualTitle, expectedTitle);
 	});
 });
